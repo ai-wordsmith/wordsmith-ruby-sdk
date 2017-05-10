6a7f24c791972b0bf4133b9bdd3339527f230431
jongaegler <jong@automatedinsights.com>
2017-05-10 14:27:28 -0400

    More private methods and test authorization

--- test/client_test.rb
+++ test/client_test.rb
@@ -1,35 +1,22 @@
 require 'test_helper'
 
 class ClientTest < Minitest::Test
-  def setup
-    Wordsmith.configure do |config|
-      config.token = 'some_token'
-    end
-  end
-
   def test_default_url
     assert_equal 'https://api.automatedinsights.com/v1', connection.url_prefix.to_s
   end
 
-  def test_url_configuration_change
+  def test_authorization_header
+    assert_equal "Bearer #{WORDSMITH_SDK_TEST_TOKEN}", authorization
+  end
+
+  def test_configuration_change
     Wordsmith.configure do |config|
+      config.token = 'some_other_token'
       config.url = 'http://www.some-other-url.com/api'
     end
 
+    assert_equal 'Bearer some_other_token', authorization
     assert_equal 'http://www.some-other-url.com/api', connection.url_prefix.to_s
-  end
-
-  def test_authorization_header
-    authorization = connection.headers['Authorization']
-    assert_equal 'Bearer some_token', authorization
-  end
-
-  def test_token_configuration_change
-    Wordsmith.configure do |config|
-      config.token = 'some_other_token'
-    end
-
-    assert_equal 'Bearer some_other_token', connection.headers['Authorization']
   end
 
   def test_content_type_header_is_json
@@ -38,15 +25,24 @@ class ClientTest < Minitest::Test
   end
 
   def test_authorization
-    # Wordsmith.configure do |config|
-    #   config.url
-    # result = Wordsmith.client.get('projects')
-    # assert_nil result
+    Wordsmith.configure { |config| config.token = 'some_other_token' }
+    begin
+      Wordsmith.client.get('projects')
+    rescue RuntimeError => e
+      assert_equal 'API authorization error.', e.message
+      return
+    end
+
+    fail 'Did not catch authorization error'
   end
 
   private
 
   def connection
     Wordsmith.client.send :connection
+  end
+
+  def authorization
+    connection.headers['Authorization']
   end
 end
